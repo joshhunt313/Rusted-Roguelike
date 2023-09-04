ba941503dc7df4baf8c084ea86e0f0656b50dd7a
Josh <joshhunt313@gmail.com>
2023-09-04 11:22:59 -0400

    Added ECS

--- src/engine.rs
+++ src/engine.rs
@@ -1,10 +1,45 @@
 use doryen_rs::{DoryenApi, Engine, UpdateEvent};
+use specs::{prelude::*, Component};
+
+
+#[derive(Component)]
+#[storage(VecStorage)]
+struct Position {
+    x: i32,
+    y: i32
+}
+
+
+#[derive(Component)]
+#[storage(VecStorage)]
+struct Player {}
+
+struct PrintPlayerPosSystem;
+impl<'a> System<'a> for PrintPlayerPosSystem {
+    type SystemData = (ReadStorage<'a, Player>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (player, mut pos): Self::SystemData) {
+        for (_, pos) in (&player, &mut pos).join() {
+            println!("Player Pos: ({:?}, {:?})", pos.x, pos.y);
+        }
+    }
+}
+
+fn move_player(new_x: i32, new_y: i32, ecs: &mut World) {
+    let (mut positions, _) = (ecs.write_storage::<Position>(), ecs.read_storage::<Player>());
+    for pos in (&mut positions).join() {
+        pos.x = new_x;
+        pos.y = new_y;
+    }
+}
+
 
 pub struct Game {
     player_pos: (i32, i32),
     mouse_pos: (f32, f32),
     console_height: u32,
-    console_width: u32
+    console_width: u32,
+    ecs: World
 }
 
 impl Game {
@@ -13,7 +48,8 @@ impl Game {
             player_pos: ((width / 2) as i32, (height / 2) as i32),
             mouse_pos: (0.0, 0.0),
             console_height: height,
-            console_width: width
+            console_width: width,
+            ecs: World::new()
         }
     }
 }
@@ -24,6 +60,12 @@ impl Engine for Game {
         api.con().register_color("white", (255, 255, 255, 255));
         api.con().register_color("red", (255, 92, 92, 255));
         api.con().register_color("blue", (192, 192, 255, 255));
+
+        self.ecs.register::<Player>();
+        self.ecs.register::<Position>();
+
+        // Create player entity
+        let _ = self.ecs.create_entity().with(Player{}).with(Position{ x: self.player_pos.0, y: self.player_pos.1 });
     }
 
     fn update(&mut self, api: &mut dyn DoryenApi) -> Option<UpdateEvent> {
@@ -64,6 +106,11 @@ impl Engine for Game {
         }
 
         self.mouse_pos = input.mouse_pos();
+
+        move_player(self.player_pos.0, self.player_pos.1, &mut self.ecs);
+
+        let mut dispatcher = DispatcherBuilder::new().with(PrintPlayerPosSystem, "print_player_pos", &[]).build();
+        dispatcher.dispatch(&self.ecs);
 
         None
     }
