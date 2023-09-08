90afa9198eb582dfd75be113077276a2feea9501
Josh <joshhunt313@gmail.com>
2023-09-08 17:39:50 -0400

    Movement is done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use tcod::{Console, input};
+use specs::{Component, VecStorage, World, WorldExt, Builder, System, WriteStorage, ReadStorage, Join, DispatcherBuilder, ReadExpect};
+use tcod::{Console, input, Renderer, Color};
 use tcod::console::{Root, FontLayout};
 use tcod::input::{Key, Mouse, Event, KeyCode};
 use std::process::exit;
@@ -6,7 +7,116 @@ use std::process::exit;
 const CONSOLE_WIDTH: u32 = 120;
 const CONSOLE_HEIGHT: u32 = 70;
 
+#[derive(Component)]
+#[storage(VecStorage)]
+struct Position {
+    x: i32,
+    y: i32
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+struct Player;
+
+#[derive(Component)]
+#[storage(VecStorage)]
+struct Entity {
+    symbol: char,
+    passable: bool
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+struct Window {
+    root: Root
+}
+
+struct MovePlayerUpSystem;
+impl<'a> System<'a> for MovePlayerUpSystem {
+    type SystemData = (WriteStorage<'a, Position>, ReadStorage<'a, Player>);
+
+    fn run(&mut self, (mut positions, players): Self::SystemData) {
+        for (pos, player) in (&mut positions, &players).join() {
+            pos.y -= 1;
+            println!("({:?}, {:?})", pos.x, pos.y)
+        }
+    }
+}
+
+struct MovePlayerDownSystem;
+impl<'a> System<'a> for MovePlayerDownSystem {
+    type SystemData = (WriteStorage<'a, Position>, ReadStorage<'a, Player>);
+
+    fn run(&mut self, (mut positions, players): Self::SystemData) {
+        for (pos, player) in (&mut positions, &players).join() {
+            pos.y += 1;
+            println!("({:?}, {:?})", pos.x, pos.y)
+        }
+    }
+}
+
+struct MovePlayerRightSystem;
+impl<'a> System<'a> for MovePlayerRightSystem {
+    type SystemData = (WriteStorage<'a, Position>, ReadStorage<'a, Player>);
+
+    fn run(&mut self, (mut positions, players): Self::SystemData) {
+        for (pos, player) in (&mut positions, &players).join() {
+            pos.x += 1;
+            println!("({:?}, {:?})", pos.x, pos.y)
+        }
+    }
+}
+
+struct MovePlayerLeftSystem;
+impl<'a> System<'a> for MovePlayerLeftSystem {
+    type SystemData = (WriteStorage<'a, Position>, ReadStorage<'a, Player>);
+
+    fn run(&mut self, (mut positions, players): Self::SystemData) {
+        for (pos, player) in (&mut positions, &players).join() {
+            pos.x -= 1;
+            println!("({:?}, {:?})", pos.x, pos.y)
+        }
+    }
+}
+
+struct RenderEntitiesSystem;
+impl<'a> System<'a> for RenderEntitiesSystem {
+    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Entity>, WriteStorage<'a, Window>);
+
+    fn run(&mut self, (positions, players, mut window): Self::SystemData) {
+        for (pos, entity) in (&positions, &players).join() {
+            println!("{:?}", (pos.x, pos.y));
+            for wind in (&mut window).join() {
+                wind.root.put_char_ex(pos.x, pos.y, entity.symbol, Color { r: 255, b: 255, g: 255 }, Color { r: 0, b: 0, g: 0});
+            }
+        }
+    }
+}
+
+struct ClearRootSystem;
+impl<'a> System<'a> for ClearRootSystem {
+    type SystemData = (WriteStorage<'a, Window>);
+
+    fn run(&mut self, mut root: Self::SystemData) {
+        for console in (&mut root).join() {
+            console.root.clear();
+        }
+    }
+}
+
+struct FlushRootSystem;
+impl<'a> System<'a> for FlushRootSystem {
+    type SystemData = (WriteStorage<'a, Window>);
+
+    fn run(&mut self, mut root: Self::SystemData) {
+        for console in (&mut root).join() {
+            console.root.flush();
+        }
+    }
+}
+
 fn main() {
+    // Initialize window
     let mut root = Root::initializer()
         .size(CONSOLE_WIDTH as i32, CONSOLE_HEIGHT as i32)
         .title("Rusted Roguelike")
@@ -14,9 +124,30 @@ fn main() {
         .renderer(tcod::Renderer::SDL)
         .init();
 
-    while !root.window_closed() {
+    // Initialize player
+    let mut world = World::new();
+    world.register::<Position>();
+    world.register::<Entity>();
+    world.register::<Player>();
+    world.register::<Window>();
+
+    // let tcod = Window { root: root };
+    world.create_entity()
+        .with(Window {root: root})
+        .build();
+
+    world.create_entity()
+        .with(Entity { symbol: '@', passable: false })
+        .with(Position { x: 1, y: 1 })
+        .with(Player)
+        .build();
+
+    loop {
         // Clear Console
-        root.clear();
+        DispatcherBuilder::new()
+            .with(ClearRootSystem, "clear_root", &[])
+            .build()
+            .dispatch(&mut world);
 
         // Handling user input
         let event = input::check_for_event(input::MOUSE | input::KEY_PRESS);
@@ -26,11 +157,38 @@ fn main() {
                 // Handle key events
                 match key_event {
                     Key { code: KeyCode::Escape, .. } => {
-                        // Handle enter key press
-                        println!("Escape key pressed");
+                        println!("Exiting game...");
                         exit(0);
                     }
-                    // Handle other key events as needed
+                    
+                    Key { code: KeyCode::Up, .. } => {
+                        let dispatcher = DispatcherBuilder::new()
+                            .with(MovePlayerUpSystem, "up", &[])
+                            .build()
+                            .dispatch(&mut world);
+                    }
+
+                    Key { code: KeyCode::Down, .. } => {
+                        let dispatcher = DispatcherBuilder::new()
+                            .with(MovePlayerDownSystem, "down", &[])
+                            .build()
+                            .dispatch(&mut world);
+                    }
+
+                    Key { code: KeyCode::Right, .. } => {
+                        let dispatcher = DispatcherBuilder::new()
+                            .with(MovePlayerRightSystem, "right", &[])
+                            .build()
+                            .dispatch(&mut world);
+                    }
+
+                    Key { code: KeyCode::Left, .. } => {
+                        let dispatcher = DispatcherBuilder::new()
+                            .with(MovePlayerLeftSystem, "left", &[])
+                            .build()
+                            .dispatch(&mut world);
+                    }
+
                     _ => {}
                 }
             }
@@ -45,10 +203,15 @@ fn main() {
             _ => {}
         }
        
-        // Updating the gamestate
         // Rendering the results
-        
+        DispatcherBuilder::new()
+            .with(RenderEntitiesSystem, "render_entities", &[])
+            .build()
+            .dispatch(&mut world);
 
-        root.flush();
+        DispatcherBuilder::new()
+            .with(FlushRootSystem, "flush_root", &[])
+            .build()
+            .dispatch(&mut world);
     }
 }
